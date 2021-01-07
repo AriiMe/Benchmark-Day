3fa55fb7d5cfed472862b4d7dadd274b3d9bd3a4
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-01-07 16:06:39 +0100

    adding questions works

--- src/questions/index.js
+++ src/questions/index.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const { readQuestions, writeQuestions } = require("../lib/utilities");
 const Router = express.Router();
 const uniqid = require("uniqid");
@@ -29,5 +30,51 @@ Router.delete("/:questionID", async (req, res, next) => {
         res.send(error);
     }
 });
+
+Router.post(
+    "/",
+    [
+        check("duration")
+            .exists()
+            .isInt()
+            .withMessage("give a duration number in sec"),
+        check("text")
+            .exists()
+            .isLength({ min: 4 })
+            .withMessage("write the question"),
+        check("answers")
+            .exists()
+            .isArray({ min: 4, max: 4 })
+            .withMessage("write the 4 answers"),
+        check("answers.*.isCorrect")
+            .exists()
+            .isBoolean()
+            .withMessage("true or false"),
+        check("answers.*.text").exists().withMessage("give text for the answers"),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            console.log(errors)
+            res.send(errors)
+        } else {
+            try {
+                const questionDB = await readQuestions();
+                questionDB.push({
+                    ...req.body,
+                    _id: uniqid(),
+                });
+                await writeQuestions(questionDB);
+                res.send("Question added")
+                console.log(req.body)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+);
+
+Router.put("/")
 
 module.exports = Router;
