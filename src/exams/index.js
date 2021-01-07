fc91d21ce2ec41d1257016bf7cdf7d85e1412018
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-01-07 12:05:45 +0100

    s

--- src/exams/index.js
+++ src/exams/index.js
@@ -47,4 +47,22 @@ Router.post("/start", async (req, res) => {
     }
 });
 
+Router.post('/start', async (req, res) => { })
+
+Router.post('/examID/answer', async (req, res) => {
+    try {
+        const examsDB = await readExam
+        const selectedExamIndex = examsDB.findIndex((exam) => exam._id === req.params.examID)
+        if (selectedExamIndex !== -1) {
+            examsDB[selectedExamIndex].questions[req.body.question].providedAnswer = req.body.answer
+            await writeExam(examsDB)
+            res.send("answer recieved")
+        } else {
+            res.send("Can't find exam bruv")
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 module.exports = Router;
