59eecaf75445fbb7673404a59ba7c2f81a99554a
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-01-07 17:06:21 +0100

    i don't remember what i changed anymore

--- src/questions/index.js
+++ src/questions/index.js
@@ -53,10 +53,10 @@ Router.post(
         check("answers.*.text").exists().withMessage("give text for the answers"),
     ],
     async (req, res) => {
-        const errors = validationResult(req)
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            console.log(errors)
-            res.send(errors)
+            console.log(errors);
+            res.send(errors);
         } else {
             try {
                 const questionDB = await readQuestions();
@@ -65,16 +65,62 @@ Router.post(
                     _id: uniqid(),
                 });
                 await writeQuestions(questionDB);
-                res.send("Question added")
-                console.log(req.body)
+                res.send("Question added");
+                console.log(req.body);
             } catch (error) {
                 console.log(error);
             }
         }
     }
-
 );
 
-Router.put("/")
+Router.put(
+    "/:questionID",
+    [
+        check("duration")
+            .exists()
+            .isInt()
+            .withMessage("give a duration number in sec"),
+        check("text")
+            .exists()
+            .isLength({ min: 4 })
+            .withMessage("write the question"),
+        check("answers")
+            .exists()
+            .isArray({ min: 4, max: 4 })
+            .withMessage("write the 4 answers"),
+        check("answers.*.isCorrect")
+            .exists()
+            .isBoolean()
+            .withMessage("true or false"),
+        check("answers.*.text").exists().withMessage("give text for the answers"),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            res.send(errors);
+        } else {
+            try {
+                const questionDB = await readQuestions();
+
+                const selectedIndex = questionDB.findIndex(
+                    (question) => question._id === req.params.questionID
+                );
+                if (selectedIndex !== -1) {
+                    questionDB[selectedIndex] = { ...questionDB[selectedIndex], ...req.body, }
+                    await writeQuestions(questionDB)
+                    res.send("question edited")
+                } else {
+                    console.log("incorrect ID")
+                    res.status(404).send("incorrect ID")
+                }
+            } catch (error) {
+                console.log(error);
+                res.send(error);
+            }
+        }
+    }
+);
 
 module.exports = Router;
